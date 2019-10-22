bc2607c890e04266d57695f9689ab3276ad51257
Mahdi Chtioui <mahdichtioui19@gmail.com>
2019-10-22 12:46:51 +0100

    adding the age to the user

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,8 @@
 exports.getUsers = (req, res, next) => {
     res.status(200).json({
         users: [
-            { first_name: 'John', last_name: 'Merphy', account: 'Premium'},
-            { first_name: 'Meleny', last_name: 'Jedah', account: 'Free'}
+            { first_name: 'John', last_name: 'Merphy', account: 'Premium', age: 23},
+            { first_name: 'Meleny', last_name: 'Jedah', account: 'Free', age: 25}
         ]
     });
 };
@@ -11,9 +11,10 @@ exports.createUser = (req, res, next) => {
     const first_name = req.body.first_name;
     const last_name = req.body.last_name;
     const account = req.body.account
+    const age = req.body.age
     //create user
     res.status(201).json({
         message: 'user created successfully!',
-        user: {id: new Date().toISOString(), first_name: first_name, last_name: last_name, account: account}
+        user: {id: new Date().toISOString(), first_name: first_name, last_name: last_name, account: account, age: age}
     });
 };
